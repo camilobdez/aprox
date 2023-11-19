1fe8da71d00e658d15c009e0a26f809a4a0ee149
camilo <cbermudezcol@gmail.com>
2023-11-19 13:55:15 -0500

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      <h2>Home</h2>
-      
+    <div className='home'>
+      <Link to="/methods">
+        <img src="/home.jpg" style={{ width: '90%', margin: 'auto', display: 'block' }} />
+      </Link>
     </div>
   );
 };
